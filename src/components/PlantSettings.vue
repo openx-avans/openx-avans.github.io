ce7f454fe578ebdb5a23dfd02b728d67015f3367
duncte123 <contact@duncte123.me>
2022-12-14 09:24:20 +0100

    Add settings

--- src/components/PlantSettings.vue
+++ src/components/PlantSettings.vue
@@ -1,11 +1,62 @@
 <script>
 export default {
-
-}
+  props: {
+    value: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+  watch: {
+    plant: {
+      deep: true,
+      handler() {
+        this.$emit('input', this.plant);
+      },
+    },
+  },
+  data() {
+    return {
+     plant: this.value,
+    }
+  },
+};
 </script>
 
 <template>
-  <div>
-    TODO: move plant settings here
+  <div class="card mb-3 shadow-sm">
+    <div class="card-header">
+      <a class="d-flex align-items-center" data-bs-toggle="collapse" href="#plant1">
+        <i class="fa-solid fa-chevron-right ms-1"></i>
+        <h3 class="ms-4 mb-0 mt-2">Plant {{ index + 1 }}</h3>
+      </a>
+    </div>
+    <div id="plant1" class="collapse m-4 card-body">
+      <div class="d-flex flex-column gap-4">
+        <div>
+          <div class="d-flex flex-row justify-content-between align-items-center">
+            <h2>Grondvochtigheid</h2>
+            <h2>(hoger is vochtiger)</h2>
+          </div>
+          <div class="d-flex flex-row gap-4 align-items-center">
+            <input type="number" class="rounded" v-model="plant.moisture" />
+            <input type="number" class="rounded" disabled value="678" />
+          </div>
+        </div>
+        <div>
+          <div class="d-flex flex-row justify-content-between align-items-center">
+            <h2>Hoeveelheid licht</h2>
+            <h2>(hoger is intenser)</h2>
+          </div>
+          <div class="d-flex flex-row gap-4 align-items-center">
+            <input type="number" class="rounded" v-model="plant.light" />
+            <input type="number" class="rounded" disabled value="2200" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,5 +1,10 @@
 <script>
+import PlantSettings from '@/components/PlantSettings.vue';
+
 export default {
+  components: {
+    PlantSettings,
+  },
   data: () => ({
     plants: [
       {
@@ -7,10 +12,20 @@ export default {
         light: 1342,
       }
     ],
+    settings: {
+      measureInterval: 20,
+      gameDuration: 30,
+      waterUsage: 0.25,
+      powerUsage: 5,
+      transmitInterval: 5,
+    },
   }),
   methods: {
     logout() {
       // TODO: stuff
+    },
+    save() {
+      alert('send to ESP if connected');
     },
   },
 };
@@ -33,38 +48,11 @@ export default {
         </h1>
         <hr>
         <!-- Foreach plant -->
-        <div class="card mb-3 shadow-sm" v-for="(plant, i) in plants" :key="i">
-          <div class="card-header">
-            <a class="d-flex align-items-center" data-bs-toggle="collapse" href="#plant1">
-              <i class="fa-solid fa-chevron-right ms-1"></i>
-              <h3 class="ms-4 mb-0 mt-2">Plant {{ i + 1 }}</h3>
-            </a>
-          </div>
-          <div id="plant1" class="collapse m-4 card-body">
-            <div class="d-flex flex-column gap-4">
-              <div>
-                <div class="d-flex flex-row justify-content-between align-items-center">
-                  <h2>Grondvochtigheid</h2>
-                  <h2>(hoger is vochtiger)</h2>
-                </div>
-                <div class="d-flex flex-row gap-4 align-items-center">
-                  <input type="number" class="rounded" v-model="plant.moisture" />
-                  <input type="number" class="rounded" disabled value="678" />
-                </div>
-              </div>
-              <div>
-                <div class="d-flex flex-row justify-content-between align-items-center">
-                  <h2>Hoeveelheid licht</h2>
-                  <h2>(hoger is intenser)</h2>
-                </div>
-                <div class="d-flex flex-row gap-4 align-items-center">
-                  <input type="number" class="rounded" v-model="plant.light" />
-                  <input type="number" class="rounded" disabled value="2200" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PlantSettings v-for="(_, i) in plants"
+                       :key="i"
+                       :index="i"
+                       v-model="plants[i]"
+        />
         <!-- End foreach -->
       </section>
       <section>
@@ -74,41 +62,41 @@ export default {
           <div>
             <h2>Meet interval</h2>
             <div class="d-flex flex-row gap-4 align-items-center">
-              <input name="measurementInterval" type="number" class="rounded" value="20" />
+              <input type="number" class="rounded" v-model="settings.measureInterval" />
               <p class="mb-0">Seconden</p>
             </div>
           </div>
           <div>
             <h2>Spelduur</h2>
             <div class="d-flex flex-row gap-4 align-items-center">
-              <input name="gameDuration" type="number" class="rounded" value="30" />
+              <input type="number" class="rounded" v-model="settings.gameDuration" />
               <p class="mb-0">Seconden</p>
             </div>
           </div>
           <div>
             <h2>Waterverbruik</h2>
             <div class="d-flex flex-row gap-4 align-items-center">
-              <input name="waterUsage" type="number" class="rounded" value="0.25" />
+              <input type="number" class="rounded" v-model="settings.waterUsage" />
               <p class="mb-0">Liter per uur</p>
             </div>
           </div>
           <div>
             <h2>Stroomverbruik</h2>
             <div class="d-flex flex-row gap-4 align-items-center">
-              <input name="powerUsage" type="number" class="rounded" value="5" />
+              <input type="number" class="rounded" v-model="settings.powerUsage" />
               <p class="mb-0">Procent per uur</p>
             </div>
           </div>
           <div>
             <h2>Data verzend interval</h2>
             <div class="d-flex flex-row gap-4 align-items-center">
-              <input name="dataTransmitInterval" type="number" class="rounded" value="5" />
+              <input type="number" class="rounded" v-model="settings.transmitInterval" />
               <p class="mb-0">Minuten</p>
             </div>
           </div>
         </div>
       </section>
-      <button id="save-button" type="submit" class="btn btn-success rounded-circle shadow">
+      <button type="button" class="btn btn-success rounded-circle shadow" @click="save">
         <i class="fa-solid fa-check"></i>
       </button>
     </form>
